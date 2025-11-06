8d5015f2d23006a798a028f900a9721c917f4e43
Jakub Jarosz <jakub@jarosz.dev>
2025-11-06 21:15:19 +0000

    cleanup tls_utils
    Signed-off-by: Jakub Jarosz <jakub@jarosz.dev>

--- tls_utils.go
+++ tls_utils.go
@@ -9,32 +9,29 @@ import (
 )
 
 // LoadCertPool loads a certificate store from a file into a CertPool object.
-func LoadCertPool(filePath string) (cp *x509.CertPool, err error) {
+func LoadCertPool(filePath string) (*x509.CertPool, error) {
 	var buf []byte
 
 	// read the entire cert store, which may contain zero, one
 	// or more certificates
-	buf, err = os.ReadFile(filePath)
+	buf, err := os.ReadFile(filePath)
 	if err != nil {
-		return
+		return nil, err
 	}
 
 	if len(buf) == 0 {
-		err = fmt.Errorf("%v: empty file", filePath)
-		return
+		return nil, fmt.Errorf("%v: empty file", filePath)
 	}
 
 	// add these certs to the pool
-	cp = x509.NewCertPool()
+	cp := x509.NewCertPool()
 	cp.AppendCertsFromPEM(buf)
 
 	// let the caller know if no usable certificate was found
 	if len(cp.Subjects()) == 0 {
-		err = fmt.Errorf("%v: no certificate found", filePath)
-		return
+		return nil, fmt.Errorf("%v: no certificate found", filePath)
 	}
-
-	return
+	return cp, nil
 }
 
 // tlsSockWrapper wraps a TLS socket to work around odd error handling in
@@ -45,71 +42,53 @@ type tlsSockWrapper struct {
 	sock net.Conn
 }
 
-func newTLSSockWrapper(sock net.Conn) (tsw *tlsSockWrapper) {
-	tsw = &tlsSockWrapper{
+func newTLSSockWrapper(sock net.Conn) *tlsSockWrapper {
+	return &tlsSockWrapper{
 		sock: sock,
 	}
-
-	return
 }
 
-func (tsw *tlsSockWrapper) Read(buf []byte) (rlen int, err error) {
-	rlen, err = tsw.sock.Read(buf)
-
-	return
+func (tsw *tlsSockWrapper) Read(buf []byte) (int, error) {
+	return tsw.sock.Read(buf)
 }
 
-func (tsw *tlsSockWrapper) Write(buf []byte) (wlen int, err error) {
-	wlen, err = tsw.sock.Write(buf)
-
-	// since write timeouts corrupt the internal state of TLS sockets,
-	// any subsequent read/write operation will fail and return the same write
-	// timeout error (see https://pkg.go.dev/crypto/tls#Conn.SetWriteDeadline).
-	// this isn't all that helpful to clients, which may be tricked into
-	// retrying forever, treating timeout errors as transient.
-	// to avoid this, close the TLS socket after the first write timeout.
-	// this ensures that clients 1) get a timeout error on the first write timeout
-	// and 2) get an ErrNetClosing "use of closed network connection" on subsequent
-	// operations.
+func (tsw *tlsSockWrapper) Write(buf []byte) (int, error) {
+	wlen, err := tsw.sock.Write(buf)
 	if err != nil && os.IsTimeout(err) {
+		// since write timeouts corrupt the internal state of TLS sockets,
+		// any subsequent read/write operation will fail and return the same write
+		// timeout error (see https://pkg.go.dev/crypto/tls#Conn.SetWriteDeadline).
+		// this isn't all that helpful to clients, which may be tricked into
+		// retrying forever, treating timeout errors as transient.
+		// to avoid this, close the TLS socket after the first write timeout.
+		// this ensures that clients 1) get a timeout error on the first write timeout
+		// and 2) get an ErrNetClosing "use of closed network connection" on subsequent
+		// operations.
 		tsw.sock.Close()
 	}
-
-	return
+	return wlen, nil
 }
 
-func (tsw *tlsSockWrapper) Close() (err error) {
-	err = tsw.sock.Close()
-
-	return
+func (tsw *tlsSockWrapper) Close() error {
+	return tsw.sock.Close()
 }
 
-func (tsw *tlsSockWrapper) SetDeadline(deadline time.Time) (err error) {
-	err = tsw.sock.SetDeadline(deadline)
-
-	return
+func (tsw *tlsSockWrapper) SetDeadline(deadline time.Time) error {
+	return tsw.sock.SetDeadline(deadline)
 }
 
-func (tsw *tlsSockWrapper) SetReadDeadline(deadline time.Time) (err error) {
-	err = tsw.sock.SetReadDeadline(deadline)
-
-	return
+func (tsw *tlsSockWrapper) SetReadDeadline(deadline time.Time) error {
+	return tsw.sock.SetReadDeadline(deadline)
 }
 
-func (tsw *tlsSockWrapper) SetWriteDeadline(deadline time.Time) (err error) {
-	err = tsw.sock.SetWriteDeadline(deadline)
-
-	return
+func (tsw *tlsSockWrapper) SetWriteDeadline(deadline time.Time) error {
+	return tsw.sock.SetWriteDeadline(deadline)
 }
 
-func (tsw *tlsSockWrapper) LocalAddr() (addr net.Addr) {
-	addr = tsw.sock.LocalAddr()
-
-	return
+func (tsw *tlsSockWrapper) LocalAddr() net.Addr {
+	return tsw.sock.LocalAddr()
 }
 
-func (tsw *tlsSockWrapper) RemoteAddr() (addr net.Addr) {
-	addr = tsw.sock.RemoteAddr()
-
-	return
+func (tsw *tlsSockWrapper) RemoteAddr() net.Addr {
+	return tsw.sock.RemoteAddr()
 }
